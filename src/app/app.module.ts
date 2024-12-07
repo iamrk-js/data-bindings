065cdf1fc084ed62ed726e2ea25571a2b045b2e3
iamrk-js <88290009+iamrk-js@users.noreply.github.com>
2024-12-07 11:59:48 +0530

    setup a angular project for data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './shared/components/product/product.component';
+import { TestComponent } from './shared/components/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
